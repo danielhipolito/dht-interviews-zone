7e2ce5415ca736501975edbbc5a19958ea069bec
Danielhipolito <daniel@healonsite.mx>
2021-07-10 18:14:58 -0500

    Added PropTypes to AnswerBox Component

--- src/components/AnswerBox.js
+++ src/components/AnswerBox.js
@@ -3,20 +3,31 @@ import PropTypes from 'prop-types';
 
 const AnswerBox = props => {
     return <div className = "row d-flex justify-content-end">
-        <div className = "p-1 col-lg-4 col-md-7 col-10 runded-circle text-center">
+        <div className = "p-1 col-lg-5 col-md-7 col-11 runded-circle text-center">
             <Card customClass = "bg-primary p-2">
-                <p className = "m-0">{props.answer} </p>
+                <div className = "row d-flex align-items-center">
+                    <div className = "col-md-9 col-8">
+                        <p className = "m-0">{props.answer} </p>
+                    </div>
+                    <div className = "col-md-2 col-3">
+                        <button className = "btn border border-dark" onClick = {props.onEditing}>
+                            Editar
+                        </button>
+                    </div>
+                </div>
             </Card>
         </div>
     </div>;
 };
 
 AnswerBox.defaultProps = {
-    answer: 'Daniel Hipolito Tolentino'
+    answer: 'Daniel Hipolito Tolentino',
+    onEditing: () => alert("No tengo función establecida")
 };
 
 AnswerBox.propTypes = {
-    answer: PropTypes.string.isRequired
+    answer: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func
 };
 
 
